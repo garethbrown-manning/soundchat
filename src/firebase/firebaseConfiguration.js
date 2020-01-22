dd9f7a703e4083dc81ce81786a8bca60a615c34e
Gareth Brown <gareth.brown@bjss.com>
2020-01-22 17:01:46 +0000

    U3M4 - Configuring Firebase in our project

---
+++ src/firebase/firebaseConfiguration.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5CdhwlU7WpVcuAuqjxBQX8KuallsOAhM",
+  authDomain: "sound-chat-526d0.firebaseapp.com",
+  databaseURL: "https://sound-chat-526d0.firebaseio.com",
+  projectId: "sound-chat-526d0",
+  storageBucket: "sound-chat-526d0.appspot.com",
+  messagingSenderId: "590691236668",
+  appId: "1:590691236668:web:27270667fafdd7af01fc39"
+};
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
