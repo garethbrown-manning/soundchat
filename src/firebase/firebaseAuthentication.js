28e78d42136b26fe2cb414fa435a49a8cd63b4ff
Gareth Brown <gareth.brown@bjss.com>
2020-03-14 17:40:43 +0000

    U5M5 - Authenticating with Email
    * Also includes changes for U5U4 - Authenticating with Twitter

--- src/firebase/firebaseAuthentication.js
+++ src/firebase/firebaseAuthentication.js
@@ -15,8 +15,27 @@ export const facebookSignin = () => {
     .catch((error) => console.error('There was an error when signing in with Facebook: ', error));
 }
 
+export const twitterSignin = () => {
+  const provider = new firebase.auth.TwitterAuthProvider();
+  firebase.auth().signInWithPopup(provider)
+    .then((result) => console.log('successfully logged in: ', result.user.displayName))
+    .catch((error) => console.error('There was an error when signing in with Twitter: ', error));
+}
+
 export const signOut = () => {
   firebase.auth().signOut()
     .then(() => console.log('User successfully signed out'))
     .catch((error) => console.error('There was an error when signing out: ', error));
+}
+
+export const emailSignin = (email, password) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(() => console.log('User successfully signed in with email and password.'))
+    .catch((error) => console.error('There was an error while signing in with email and password: ', error));
+}
+
+export const createEmailSigninAccount = (email, password) => {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(() => console.log('User successfully created an account with email and password'))
+    .catch((error) => console.error('There was an error while creating a new user with email and password: ', error));
 }
