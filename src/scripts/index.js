ec14543105249025d37863f2d19812ff8a188764
Gareth Brown <gareth.brown@bjss.com>
2020-03-14 16:03:03 +0000

    U5M3 - Authenticating with Facebook

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,9 @@
 import '../firebase/firebaseConfiguration';
 import { assignClick, initializeSigninButtons } from './utilities';
-import { googleSignin, signOut } from '../firebase/firebaseAuthentication';
+import { googleSignin, signOut, facebookSignin } from '../firebase/firebaseAuthentication';
 
 initializeSigninButtons();
 
 assignClick('signin-google', googleSignin);
+assignClick('signin-facebook', facebookSignin);
 assignClick('appbar-signout-button', signOut);
